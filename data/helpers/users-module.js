7a5fc83ef7d89b5377ac714a1a90ec0bef414389
joepak <josephpak2014@gmail.com>
2019-04-01 17:33:37 -0400

    Register endpoint

---
+++ data/helpers/users-module.js
@@ -0,0 +1,24 @@
+const db = require('./dbConfig.js');
+
+module.exports = {
+    addUser,
+    getUsers,
+    getUserBy,
+}
+
+function getUsers() {
+    return db('users')
+}
+
+function getUserBy(filter) {
+    return db('users')
+        .where(filter)     
+}
+
+async function addUser(user) {
+    const [id] = await db('users')
+        .insert(user);
+
+    return getUserBy({id})
+}
+
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors')
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const Users = require('./data/helpers/users-module.js')
 
 const server = express();
 
@@ -8,13 +11,28 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors())
 
-server.post('/api/register', (req,res) => {
-    let user = req.body;
-    let { username, password } = user
+server.post('/api/register', async (req,res) => { 
+    try {
+        let user = req.body;
+        const hash = bcrypt.hashSync(user.password, 4)
+        user.password = hash;
 
-    const hash = bcrypt.hashSync(password, 4)
-    password = hash;
+        const newUser = await Users.addUser(user)
+        res.status(201).json(newUser)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
+})
 
+server.get('/api/users', async (req,res) => {
+    try {
+        const users = await Users.getUsers()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
 })
 
 module.exports = server;
