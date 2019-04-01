55437ebcdf7fc3c3d058142e6c63c502b5aa648c
joepak <josephpak2014@gmail.com>
2019-04-01 15:51:33 -0400

    Users table schema

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors')
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors())
+
+server.post('/api/register', (req,res) => {
+    let user = req.body;
+    let { username, password } = user
+
+    const hash = bcrypt.hashSync(password, 4)
+    password = hash;
+
+})
 
 module.exports = server;
